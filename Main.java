70a236d023fbeacd49958935720ae3105f39f396
XomaUA <kovalnick@gmail.com>
2020-03-27 10:50:14 +0200

    Commit2

--- Main.java
+++ Main.java
@@ -7,25 +7,29 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("What is your price for the country #1?");
         int amount1 = Integer.parseInt(br.readLine());
-        // String c1 = br.readLine();
+
         System.out.println("What is your price for the country #2?");
         int amount2 = Integer.parseInt(br.readLine());
-        // String c2 = br.readLine();
+
         System.out.println("What is your price for the country #3?");
         int amount3 = Integer.parseInt(br.readLine());
-        //  String c3 = br.readLine();
+
         System.out.println("How long for the country #1?");
         int amount4 = Integer.parseInt(br.readLine());
-        // String t1 = br.readLine();
-        System.out.println("How long for the country #1?");
+
+        System.out.println("How long for the country #2?");
         int amount5 = Integer.parseInt(br.readLine());
-        // String t2 = br.readLine();
-        System.out.println("How long for the country #1?");
+
+        System.out.println("How long for the country #3?");
         int amount6 = Integer.parseInt(br.readLine());
-        // String t3 = br.readLine();
-        System.out.println("Cost #1 is " + (amount1 * amount4));
-        System.out.println("Cost #2 is " + (amount2 * amount5));
-        System.out.println("Cost #3 is " + (amount3 * amount6));
+        int Total1 = amount1 * amount4;
+        int Total2 = amount2 * amount5;
+        int Total3 = amount3 * amount6;
+        int TotalTotal = Total1 * Total2 * Total3;
+        System.out.println("Cost #1 is " + Total1);
+        System.out.println("Cost #2 is " + Total2);
+        System.out.println("Cost #3 is " + Total3);
+        System.out.println("The total amount for all calls from 3 countries: " + TotalTotal);
 
     }
 }
